f9c71c355f5f19145581af64b234a74e7743cd2c
Emmanuel <Emmanuelagyeikumi@gmail.com>
2023-12-23 22:59:07 +0000

    Update status code in the response for user registration

--- routes/manageevents.js
+++ routes/manageevents.js
@@ -29,7 +29,7 @@ router.post("/register", async (req, res) => {
 
     await newUser.save();
 
-    res.status(201).json({ message: "User registered successfully" });
+    res.status(201).json({statusCode: 200, message: "User registered successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
